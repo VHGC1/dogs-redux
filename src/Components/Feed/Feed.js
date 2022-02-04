24d8ca27ca37de2903778068bf193e0b7371a506
Vitor <vitorjjhg@hotmail.com>
2022-02-04 11:54:01 -0300

    feed

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,21 +1,30 @@
-import React, { useEffect, useState } from "react";
-import FeedModal from "./FeedModal";
-import FeedPhotos from "./FeedPhotos";
-import PropTypes from "prop-types";
+import React from 'react';
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadNewPhotos, resetFeedState } from '../../store/feed';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
 
 const Feed = ({ user }) => {
-  const [modalPhoto, setModalPhoto] = useState(null);
-  const [pages, setPages] = useState([1]);
-  const [infinite, setInfinite] = useState(true);
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+  const { infinite, loading, list, error } = useSelector((state) => state.feed);
+  const dispatch = useDispatch();
 
-  useEffect(() => {
+  React.useEffect(() => {
+    dispatch(resetFeedState());
+    dispatch(loadNewPhotos({ user, total: 6 }));
+  }, [dispatch, user]);
+
+  React.useEffect(() => {
     let wait = false;
     function infiniteScroll() {
       if (infinite) {
         const scroll = window.scrollY;
         const height = document.body.offsetHeight - window.innerHeight;
         if (scroll > height * 0.75 && !wait) {
-          setPages((pages) => [...pages, pages.length + 1]);
+          dispatch(loadNewPhotos({ user, total: 6 }));
           wait = true;
           setTimeout(() => {
             wait = false;
@@ -24,28 +33,23 @@ const Feed = ({ user }) => {
       }
     }
 
-    window.addEventListener("wheel", infiniteScroll);
-    window.addEventListener("scroll", infiniteScroll);
+    window.addEventListener('wheel', infiniteScroll);
+    window.addEventListener('scroll', infiniteScroll);
     return () => {
-      window.removeEventListener("wheel", infiniteScroll);
-      window.removeEventListener("scroll", infiniteScroll);
+      window.removeEventListener('wheel', infiniteScroll);
+      window.removeEventListener('scroll', infiniteScroll);
     };
-  }, [infinite]);
+  }, [infinite, dispatch, user]);
 
   return (
     <div>
       {modalPhoto && (
-        <FeedModal setModalPhoto={setModalPhoto} photo={modalPhoto} />
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
       )}
-      {pages.map((page) => (
-        <FeedPhotos
-          page={page}
-          key={page}
-          user={user}
-          setModalPhoto={setModalPhoto}
-          setInfinite={setInfinite}
-        />
-      ))}
+      {list.length > 0 && <FeedPhotos setModalPhoto={setModalPhoto} />}
+      {loading && <Loading />}
+      {error && <Error error={error} />}
+
       {!infinite && !user && (
         <p
           style={{
