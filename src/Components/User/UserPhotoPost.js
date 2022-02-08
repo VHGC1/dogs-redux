91359b24aef1fa88dff4acb8ff3247534580b661
Vitor <vitorjjhg@hotmail.com>
2022-02-08 10:03:55 -0300

    photoPost

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -1,38 +1,37 @@
-import React, { useEffect, useState } from "react";
-import styles from "./UserPhotoPost.module.css";
-import Input from "../Forms/Input";
-import Button from "../Forms/Button";
-import useForm from "../../Hooks/useForm";
-import useFetch from "../../Hooks/useFetch";
-import { PHOTO_POST } from "../../api";
-import Error from "../Helper/Error";
-import { useNavigate } from "react-router-dom";
-import Head from "../Helper/Head"
+import React from 'react';
+import styles from './UserPhotoPost.module.css';
+import useForm from '../../Hooks/useForm';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import Error from '../Helper/Error';
+import { useNavigate } from 'react-router-dom';
+import Head from '../Helper/Head';
+import { useDispatch, useSelector } from 'react-redux';
+import { photoPost } from '../../store/photoPost';
 
 const UserPhotoPost = () => {
   const nome = useForm();
-  const peso = useForm("number");
-  const idade = useForm("number");
-  const [img, setImg] = useState({});
-  const { data, error, loading, request } = useFetch();
-
+  const peso = useForm('number');
+  const idade = useForm('number');
+  const [img, setImg] = React.useState({});
+  const { data, error, loading } = useSelector((state) => state.photoPost);
+  const { token } = useSelector((state) => state.token.data);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (data) navigate("/conta");
+  React.useEffect(() => {
+    if (data) navigate('/conta');
   }, [data, navigate]);
 
   function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData();
-    formData.append("img", img.raw);
-    formData.append("nome", nome.value);
-    formData.append("peso", peso.value);
-    formData.append("idade", idade.value);
+    formData.append('img', img.raw);
+    formData.append('nome', nome.value);
+    formData.append('peso', peso.value);
+    formData.append('idade', idade.value);
 
-    const token = window.localStorage.getItem("token");
-    const { url, options } = PHOTO_POST(formData, token);
-    request(url, options);
+    dispatch(photoPost({ formData, token }));
   }
 
   function handleImgChange({ target }) {
@@ -44,7 +43,7 @@ const UserPhotoPost = () => {
 
   return (
     <section className={`${styles.photoPost} animeLeft`}>
-      <Head title="Poste a sua foto" />
+      <Head title="Poste sua foto" />
       <form onSubmit={handleSubmit}>
         <Input label="Nome" type="text" name="nome" {...nome} />
         <Input label="Peso" type="number" name="peso" {...peso} />
@@ -57,13 +56,12 @@ const UserPhotoPost = () => {
           onChange={handleImgChange}
         />
         {loading ? (
-          <Button disabled>Carregando...</Button>
+          <Button disabled>Enviando...</Button>
         ) : (
           <Button>Enviar</Button>
         )}
         <Error error={error} />
       </form>
-          {img.preview && console.log((img.preview).slice("blob"))}
       <div>
         {img.preview && (
           <div
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,15 +2,16 @@ import {
   combineReducers,
   configureStore,
   getDefaultMiddleware,
-} from "@reduxjs/toolkit";
-import feed from "./feed";
-import photo from "./photo";
-import token from "./token";
-import ui from "./ui";
-import user from "./user";
+} from '@reduxjs/toolkit';
+import photo from './photo';
+import photoPost from './photoPost';
+import token from './token';
+import user from './user';
+import feed from './feed';
+import ui from './ui';
 
 const middleware = [...getDefaultMiddleware()];
-const reducer = combineReducers({ photo, token, user, feed, ui });
+const reducer = combineReducers({ photo, photoPost, token, user, feed, ui });
 const store = configureStore({ reducer, middleware });
 
 export default store;
